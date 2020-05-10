3d1b22d9d63b39ae18d4985e848c3c89ffe5f4d7
Tran Duc Son <sontd.hust@gmail.com>
2020-05-10 12:46:04 +0900

    Show message dialog.

--- app/www/layouts/default.vue
+++ app/www/layouts/default.vue
@@ -7,10 +7,13 @@
     </v-content>
     <v-footer>
       <div>
-        Powered by
+        <span>Powered by</span>
         <a href="https://g.co/magenta/js">magenta-js</a>,
         <a href="https://www.rust-lang.org/what/wasm">WASM by Rust</a>,
-        <a href="https://nuxtjs.org">Nuxt.js</a>
+        <a href="https://nuxtjs.org">Nuxt.js</a>.
+        <span>A huge thanks to</span>
+        <a href="https://github.com/minh93">@minh93</a>,
+        <a href="https://github.com/yuyuvn">@yuyuvn</a>.
       </div>
     </v-footer>
   </v-app>
--- app/www/pages/index.vue
+++ app/www/pages/index.vue
@@ -22,12 +22,18 @@
         </v-btn>
       </v-col>
       <v-col :cols="2">
-        <v-btn v-if="isDone" @click="save">Save</v-btn>
+        <v-btn v-if="isSuccessful" @click="save">Save</v-btn>
       </v-col>
     </v-row>
     <v-flex>
-      <player-component v-if="isDone" :player="player" />
+      <player-component v-if="isSuccessful" :player="player" />
     </v-flex>
+    <v-dialog v-model="messageDialog" max-width="300">
+      <v-card>
+        <v-card-title>Message</v-card-title>
+        <v-card-text>{{ messageText }}</v-card-text>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -54,15 +60,20 @@ export default {
   data() {
     const types = ['Youtube URL', 'Audio file']
     return {
+      // Read-only
       type: types[0],
       types,
       youtubeUrl: '',
       file: null,
+      // Process
       fileName: '',
       progress: '',
       isProcessing: false,
-      isDone: false,
+      isSuccessful: false,
       midi: null,
+      messageDialog: false,
+      messageText: '',
+      // Playing
       player: new Player()
     }
   },
@@ -73,20 +84,36 @@ export default {
   },
   methods: {
     async process(event) {
+      const showDialog = (messageText) => {
+        this.messageText = messageText
+        this.messageDialog = true
+        this.isSuccessful = false
+        this.isProcessing = false
+      }
+      this.fileName = ''
       this.progress = 'reading'
       this.isProcessing = true
-      this.isDone = false
+      this.isSuccessful = false
+      this.midi = null
+      this.messageDialog = false
+      this.messageText = ''
       let file = null
       if (this.isYoutube) {
-        const url = new URL(this.youtubeUrl)
+        let url = null
+        try {
+          url = new URL(this.youtubeUrl)
+        } catch (e) {
+          return showDialog('Invalid URL')
+        }
         const videoId = new URLSearchParams(url.search).get('v')
         const apiUrl = 'https://oggy.metalwhale.dev:8000'
         const streamUrl = `${apiUrl}/youtube_audio?video_id=${videoId}`
         const infoUrl = `${apiUrl}/youtube_info?video_id=${videoId}`
         file = await this.$axios.$get(streamUrl, { responseType: 'blob' })
-        this.$axios.$get(infoUrl).then((response) => {
-          this.fileName = response.info.title
-        })
+        const info = await this.$axios.$get(infoUrl)
+        if (info.info) {
+          this.fileName = info.info.title
+        }
       } else {
         file = this.file
         this.fileName = file.name.split('.')[0]
@@ -95,32 +122,39 @@ export default {
       const array = new Uint8Array(fileBuffer)
       // See: https://github.com/grimmdude/MidiPlayerJS/blob/master/src/player.js#L103
       if (Utils.bytesToLetters(array.subarray(0, 4)) !== 'MThd') {
-        const audio = await wasm.decode_audio(fileBuffer)
-        const rate = audio.sampleRate
-        const bufferLength = rate * 10
-        const data = new Float32Array(bufferLength)
-        const context = new OfflineAudioContext(1, rate, rate)
-        const buffer = context.createBuffer(2, bufferLength, rate)
-        const sequences = []
-        const sequencesCount = Math.ceil(audio.length / bufferLength)
-        for (let i = 0; i < sequencesCount; i++) {
-          const length = i * bufferLength
-          this.progress = `${i + 1}/${sequencesCount} sequences`
-          for (let j = 0; j < audio.numberOfChannels; j++) {
-            audio.copyFromChannel(data, j, length)
-            buffer.copyToChannel(data, j, 0)
-          }
-          sequences.push(await this.model.transcribeFromAudioBuffer(buffer))
+        let audio = null
+        try {
+          audio = await wasm.decode_audio(fileBuffer)
+        } catch (e) {
+          return showDialog('Error occurred while decoding')
         }
-        const sequence = mm.sequences.concatenate(sequences)
-        this.midi = mm.sequenceProtoToMidi(sequence)
-        fileBuffer = this.midi.buffer
+        if (audio) {
+          const rate = audio.sampleRate
+          const bufferLength = rate * 10
+          const data = new Float32Array(bufferLength)
+          const context = new OfflineAudioContext(1, rate, rate)
+          const buffer = context.createBuffer(2, bufferLength, rate)
+          const sequences = []
+          const sequencesCount = Math.ceil(audio.length / bufferLength)
+          for (let i = 0; i < sequencesCount; i++) {
+            const length = i * bufferLength
+            this.progress = `${i + 1}/${sequencesCount} sequences`
+            for (let j = 0; j < audio.numberOfChannels; j++) {
+              audio.copyFromChannel(data, j, length)
+              buffer.copyToChannel(data, j, 0)
+            }
+            sequences.push(await this.model.transcribeFromAudioBuffer(buffer))
+          }
+          const sequence = mm.sequences.concatenate(sequences)
+          this.midi = mm.sequenceProtoToMidi(sequence)
+          fileBuffer = this.midi.buffer
+        }
       } else {
         this.midi = array
       }
       this.player.loadArrayBuffer(fileBuffer)
+      this.isSuccessful = true
       this.isProcessing = false
-      this.isDone = true
     },
     save() {
       saveAs(new File([this.midi], `${this.fileName}.mid`))
